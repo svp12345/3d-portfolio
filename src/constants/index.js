7202a316c850c9a152b080e613623ef5d4bd3a7d
Shruti Pokale <64649386+svp12345@users.noreply.github.com>
2024-05-26 20:26:55 +0530

    portfolio files are added

--- src/constants/index.js
+++ src/constants/index.js
@@ -24,192 +24,190 @@ import {
   express,
   xpenser,
   ecommerce,
-  youtube
-  } from "../assets";
-  
-  export const navLinks = [
-    {
-      id: "about",
-      title: "About",
-    },
-    {
-      id: "work",
-      title: "Work",
-    },
-    {
-      id: "contact",
-      title: "Contact",
-    },
-  ];
-  
-  const services = [
-    {
-      title: "C++ Developer",
-      icon: web,
-    },
-    {
-      title: "React Developer",
-      icon: mobile,
-    },
-    {
-      title: "Full Stack Developer",
-      icon: backend,
-    },
-    {
-      title: "Writer",
-      icon: creator,
-    },
-  ];
-  
-  const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
-    {
-      name: "Redux Toolkit",
-      icon: redux,
-    },
-    {
-      name: "Tailwind CSS",
-      icon: tailwind,
-    },
-    {
-      name: "Node JS",
-      icon: nodejs,
-    },
-    {
-      name: "MongoDB",
-      icon: mongodb,
-    },
-    {
-      name: "bootstrap",
-      icon: bootstrap,
-    },
-    {
-      name: "git",
-      icon: git,
-    },
-    {
-      name: "mui",
-      icon: mui,
-    },
-    {
-      name: "express",
-      icon: express,
-    },
-  ];
-  
-  const experiences = [
-    {
-      title: "React.js Developer",
-      company_name: "CitiusTech",
-      icon: ct,
-      iconBg: "#383E56",
-      date: "July 2022 - Sept 2023",
-      points: [
-        "I have 1+ years of experience in Web Application Development using HTML,CSS,JavaScript,Reactjs",
-        "Converted design of 10+, (Figma) files into highly functional and responsive web pages using ReactJs",
-        "Worked on the designing of control system architecture for MRI-guided radiation therapy system",
-        "Optimized the Existing Code using React Hooks like useState Hook and useEffect Hook",
-        "Worked on API Integration, Designing and Creating New Components Using ReactJS, React Styled Components",
-        "Worked on the issues regarding the Redux Store and Redux Forms for cloud based application",
-      ],
-    },
-    {
-      title: "SDE Intern",
-      company_name: "ITJOBXS",
-      icon: itjobxs,
-      iconBg: "#E6DEDD",
-      date: "Jan 2024 - April 2024",
-      points: [
-        "Worked on the Design and Development part of a fully responsive webpage for a section of ITJOBXS",
-        "Worked on User Verification/Authentication and Engineering challenges of detecting and removing fake bots and posts",
-        "Integrated Recaptcha with the website - Google Layer of Protection for websites",
-      ],
-    },
-  ];
-  
-  const projects = [
-    {
-      name: "YouTube Clone",
-      description:
-        "A responsive React JS application consisting of stunning video sections, custom categories, channel pages, and you can play videos of your choice and you can search to find the content of your choice",
-      tags: [
-        {
-          name: "React",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "MUI",
-          color: "green-text-gradient",
-        },
-        {
-          name: "RapidAPI",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: youtube,
-      source_code_link: "https://github.com/svp12345/YouTube-clone",
-    },
-    {
-      name: "E-commerce App",
-      description:
-        "- Building a full-stack web application using stripe and sanity can perform operations like viewing products,placing product orders, add and edit products and cover real payment",
-      tags: [
-        {
-          name: "React",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "Stripe",
-          color: "green-text-gradient",
-        },
-        {
-          name: "Sanity",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: ecommerce,
-      source_code_link: "https://github.com/svp12345/E-commerce",
-    },
-    {
-      name: "Xpenser",
-      description:
-        "Built using ReactJS and Redux for the state management.We can Add and Delete any expense to the expense list.We can search for any expense from the list.",
-      tags: [
-        {
-          name: "React",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "Redux",
-          color: "green-text-gradient",
-        },
-        {
-          name: "Bootstrap",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: xpenser,
-      source_code_link: "https://github.com/svp12345/Expense-Tracker",
-    },
-  ];
-  
-  export { services, technologies, experiences, projects };
+  youtube,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "C++ Developer",
+    icon: web,
+  },
+  {
+    title: "React Developer",
+    icon: mobile,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: backend,
+  },
+  {
+    title: "Writer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "bootstrap",
+    icon: bootstrap,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "mui",
+    icon: mui,
+  },
+  {
+    name: "express",
+    icon: express,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "CitiusTech",
+    icon: ct,
+    iconBg: "#383E56",
+    date: "July 2022 - Present",
+    points: [
+      "Over 1+ year of experience in developing web applications using HTML,CSS,JavaScript,Reactjs",
+      "Successfully converted designs from 10+ Figma files into highly functional and responsive web pages",
+      "Contributed to the control system architecture for an MRI guided radiation therapy system, enhancing system reliability by 25%",
+      "Enhanced existing codebases by utilizing React Hooks reducing component render times by 20% and improving application performance",
+      "Resolved issues related to Redux Store and Redux Forms for a cloud-based application, increasing data consistency and reducing bugs by 40%",
+    ],
+  },
+  {
+    title: "SDE Intern",
+    company_name: "ITJOBXS",
+    icon: itjobxs,
+    iconBg: "#E6DEDD",
+    date: "Jan 2022 - April 2022",
+    points: [
+      "Led initiatives in user verification/authentication and overcame engineering challenges to detect and remove fake bots and posts. Resulted in a 30% reduction in fraudulent activities,enhancing system security and boosting user trust",
+      "Integrated Google Recaptcha with the website, adding an extra layer of protection and reducing bot activity by 35%",
+    ],
+  },
+];
+
+const projects = [
+  {
+    name: "YouTube Clone",
+    description:
+      "A responsive React JS application consisting of stunning video sections, custom categories, channel pages, and you can play videos of your choice and you can search to find the content of your choice",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MUI",
+        color: "green-text-gradient",
+      },
+      {
+        name: "RapidAPI",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: youtube,
+    source_code_link: "https://github.com/svp12345/YouTube-clone",
+  },
+  {
+    name: "E-commerce App",
+    description:
+      "- Building a full-stack web application using stripe and sanity can perform operations like viewing products,placing product orders, add and edit products and cover real payment",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Stripe",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Sanity",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: ecommerce,
+    source_code_link: "https://github.com/svp12345/E-commerce",
+  },
+  {
+    name: "Xpenser",
+    description:
+      "Built using ReactJS and Redux for the state management.We can Add and Delete any expense to the expense list.We can search for any expense from the list.",
+    tags: [
+      {
+        name: "React",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Bootstrap",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: xpenser,
+    source_code_link: "https://github.com/svp12345/Expense-Tracker",
+  },
+];
+
+export { services, technologies, experiences, projects };
